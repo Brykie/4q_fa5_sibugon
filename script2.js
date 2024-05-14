a721de95926f77c441905c7f473795425a3c1534
Brykie <143463506+Brykie@users.noreply.github.com>
2024-05-14 10:42:47 +0800

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,30 @@
+let contacts = [];
+
+function addcontact() {
+    let newcontact = document.getElementById('contact').value.trim();
+    if (newcontact !== "") {
+        contacts.push(newcontact);
+
+        if (contacts.length > 7) {
+            contacts.shift();
+        }
+    }
+
+    contactlist();
+    document.getElementById('contact').value = "";
+}
+
+function removecontact() {
+    if (contacts.length > 0)
+    contacts.pop();
+    contactlist();
+}
+
+function contactlist() {
+    let list = "<ul>";
+    contacts.forEach(contact => {output += "<li>" + contact + "</li>";});
+    list += "</ul>";
+    document.getElementById('output').innerHTML = list;
+}
+
+
